6dc744b7093928fc72bc2aa43361d3eaea525912
cleianderson <cleianderson13@gmail.com>
2022-09-13 23:54:48 -0300

    Updates

---
+++ precache-manifest.283db9957b28ecf4327eff5a9aea66a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e6bc46896d59ce4c5d4bd066f7433ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8c8709f99e87569dbea",
+    "url": "/static/css/2.4daed3a1.chunk.css"
+  },
+  {
+    "revision": "44054126e661bd8e1751",
+    "url": "/static/css/main.e855c986.chunk.css"
+  },
+  {
+    "revision": "f8c8709f99e87569dbea",
+    "url": "/static/js/2.38ad15cb.chunk.js"
+  },
+  {
+    "revision": "784bc76aeb6a148e99480ef26ca89bc7",
+    "url": "/static/js/2.38ad15cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44054126e661bd8e1751",
+    "url": "/static/js/main.b4e32642.chunk.js"
+  },
+  {
+    "revision": "44a5ff80f14cde1ccda1",
+    "url": "/static/js/runtime-main.79d7efb6.js"
+  },
+  {
+    "revision": "236b96981c225f7eed513be96780f62b",
+    "url": "/static/media/icon.236b9698.png"
+  }
+]);
