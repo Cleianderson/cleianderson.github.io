41607fc88e7e3e904ec26bee75f5c5d4ac3e172f
Cleianderson <cleianderson13@gmail.com>
2022-11-23 11:07:43 -0300

    refact: use fly io backend

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'https://backendru.herokuapp.com' })
+const api = axios.create({ baseURL: 'https://backend-rural-ts.fly.dev' })
 
 export default api
